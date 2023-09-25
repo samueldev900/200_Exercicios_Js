ac979a396e0ae1941a3f481334112d9ae57c13ac
samueldev900 <samueloliveira900@gmail.com>
2023-09-25 17:55:51 -0300

    update

--- ex034.js
+++ ex034.js
@@ -1,20 +1,3 @@
-var i =0;
-while(i <=  100){
-    i ++
-    
-    if(i % 5 ==0 && i % 3 == 0){
-
-        console.log(i,`FizzBuzz`)
-    }
-    else if(i % 5 == 0){
-        console.log(i,`Buzz`)
-    }
-    else if( i % 3 ==0){
-        console.log(i,`Fizz`)
-    }
-    else{
-        console.log(i)
-    }
-    
+for(let i = 1){
 
 }

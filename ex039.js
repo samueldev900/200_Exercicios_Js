565f5c6770c379e82de30c2e73c08d34c43491d6
samueldev900 <samueloliveira900@gmail.com>
2023-10-19 10:08:54 -0300

    Update

---
+++ ex039.js
@@ -0,0 +1,11 @@
+function fatorial(n){
+    var n1 = 1;
+
+    for(var i = n; i > 0; i--){
+         n1*=i
+        console.log(n1)
+    }
+}
+
+
+fatorial(15)
---
+++ ex040.js
@@ -0,0 +1,11 @@
+function print(n){
+var output = "";
+for (var i = n.length - 1; i >= 0; i--) {
+    output += n[i];
+}
+    console.log(output);
+}
+
+
+print('Hello')
+
